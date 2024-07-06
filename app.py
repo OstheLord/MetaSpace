b70005add267d6d29d885fcbb5368e80847729fd
OstheLord <55671621+OstheLord@users.noreply.github.com>
2024-07-06 09:35:20 -0400

    improve variables

--- app.py
+++ app.py
@@ -7,53 +7,55 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-database = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 @app.before_request
-def convert_json_request():
+def ensure_json_request():
     if request.is_json:
         request.json_data = request.get_json()
 
-class Room(database.Model):
-    room_id = database.Column(database.Integer, primary_key=True)
-    room_name = database.Column(database.String(120), nullable=False)
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
 
-class User(database.Model):
-    user_id = database.Column(database.Integer, primary_key=True)
-    username = database.Column(database.String(80), unique=True, nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
 @app.route('/rooms', methods=['POST', 'GET'])
-def room_operations():
+def handle_rooms():
     if request.method == 'POST':
-        if request.json_data:
-            new_room = Room(room_name=request.json_data['name'])
-            database.session.add(new_room)
-            database.session.commit()
+        room_data = request.json_data
+        if room_data:
+            new_room = Room(name=room_data['name'])
+            db.session.add(new_room)
+            db.session.commit()
             return jsonify({"message": "Room created successfully"}), 201
-        return jsonify({"error": "Bad request"}), 400
+        return jsonify({"error": "Bad request - missing room name"}), 400
     else:
-        all_rooms = Room.query.all()
-        return jsonify([{'id': room.room_id, 'name': room.room_name} for room in all_rooms]), 200
+        rooms = Room.query.all()
+        return jsonify([{'id': room.id, 'name': room.name} for room in rooms]), 200
 
 @app.route('/users', methods=['POST', 'GET'])
-def user_operations():
+def handle_users():
     if request.method == 'POST':
-        if request.json_data:
-            new_user = User(username=request.json_data['username'])
-            database.session.add(new_user)
-            database.session.commit()
+        user_data = request.json_data
+        if user_data:
+            new_user = User(username=user_data['username'])
+            db.session.add(new_user)
+            db.session.commit()
             return jsonify({"message": "User created successfully"}), 201
-        return jsonify({"error": "Bad request"}), 400
+        return jsonify({"error": "Bad request - missing username"}), 400
     else:
-        all_users = User.query.all()
-        return jsonify([{'id': user.user_id, 'username': user.username} for user in all_users]), 200
+        users = User.query.all()
+        return jsonify([{'id': user.id, 'username': user.username} for user in users]), 200
 
 @app.before_first_request
-def setup_database():
-    database.create_all()
+def initialize_database():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
