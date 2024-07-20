cf449e4a589ddc8e89f8fa690c0cd2e7348d5af8
OstheLord <55671621+OstheLord@users.noreply.github.com>
2024-07-20 13:11:43 -0400

    optimize

--- models.py
+++ models.py
@@ -1,13 +1,12 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_season, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker, scoped_session
 from os import getenv
 
 DATABASE_URL = getenv("DATABASE_URL", "sqlite:///virtual_space.db")
 
 engine = create_engine(DATABASE_URL, echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
+Session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 
 class User(Base):
@@ -25,9 +24,20 @@ class Room(Base):
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
     creator_id = Column(Integer, ForeignKey('users.id'))
-    creator = relationship("User", back_populates="created_rooms")
+    creator = relationship("User", back_populates="created_rooms", lazy='select')
     interactive_elements = Column(String, nullable=True)
 
-User.created_rooms = relationship("Room", order_by=Room.id, back_populates="creator")
+User.created_rooms = relationship("Room", order_by=Room.id, back_populates="creator", lazy='select')
 
 Base.metadata.create_all(engine)
+
+def add_user(username, email):
+    session = Session()
+    try:
+        session.add(User(username=username, email=email))
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
